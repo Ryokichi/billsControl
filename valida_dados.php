9cb82158ee48b78960d2573cfbf5f97bab0a3b2f
Ryokichi <ryokichi_nakamura@hotmail.com>
2016-09-26 07:38:39 -0300

    Conecta BD e valida usuario

---
+++ valida_dados.php
@@ -0,0 +1,17 @@
+<?php
+include 'Conexao.php' ;
+
+$success = false;
+$login = $_POST['USUARIO'];
+$senha = $_POST['PASS'];
+$con = new Conexao($login, $senha);
+
+$response = [
+	'usuario' => $_POST['USUARIO'],
+	'senha' => $_POST['PASS'],
+	'success' => $con->response
+	];
+
+echo json_encode($response);
+
+?>
